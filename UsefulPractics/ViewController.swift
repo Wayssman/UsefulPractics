61993dafe51d6c278e20b8de9296bcba4dbf6248
Alexandr <>
2022-10-25 19:19:03 +0300

    Deleted storyboard

---
+++ UsefulPractics/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  UsefulPractics
+//
+//  Created by Alexandr on 25.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+    }
+}
+
